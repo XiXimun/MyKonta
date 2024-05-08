75aaaf5d7bf8b0ec634a3d34f6b1e819ad16d57b
XiXimun <ximun.loyatho@gmail.com>
2024-05-08 14:37:36 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def home():
+    st.title('Home Page')
+    st.write('Welcome to the Home page!')
+    st.write('Navigate to the About page using the sidebar.')
+
+def about():
+    st.title('About Page')
+    st.write('This is the About page.')
+    st.write('Feel free to learn more about us here!')
+
+def main():
+    st.sidebar.title('Navigation')
+    page = st.sidebar.radio("Go to", ["Home", "About"])
+
+    if page == "Home":
+        home()
+    elif page == "About":
+        about()
+
+if __name__ == "__main__":
+    main()
